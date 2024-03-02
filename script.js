0062e47b74276e07027c9dc7689f0ab6ffae97d1
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-02 04:52:47 -0300

    phone css #1

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const SidebarButton = document.body.querySelector(".Sidebartoggle"), Sidebar = document.body.querySelector(".sidebar");
+const SidebarButton = document.body.querySelector(".Sidebartoggle"),
+    Sidebar = document.body.querySelector(".sidebar"),
+    //themes buttons
+    cyberpunk_theme = document.body.querySelector(".cyberPunk_button"),
+    solarPunk_theme = document.body.querySelector(".SolarPunk_button"),
+    steamPunk_theme = document.body.querySelector(".SteamPunk_button")
+    ;
 
 SidebarButton.addEventListener("click", () => {
     Sidebar.classList.toggle("close");
