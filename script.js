9eddba2ddcf2eaf83212bd6c6f587a5e5e2842c1
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-03 17:12:29 -0300

    .

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const SidebarButton = document.body.querySelector(".Sidebartoggle"),
     //themes buttons
     cyberpunk_theme = document.body.querySelector(".cyberPunk_button"),
     solarPunk_theme = document.body.querySelector(".SolarPunk_button"),
-    steamPunk_theme = document.body.querySelector(".SteamPunk_button")
-    ;
+    steamPunk_theme = document.body.querySelector(".SteamPunk_button"),
+    inputDetection = document.body.querySelector(".SearchInput"),
+    searchDefinitions = document.querySelector('.search_definitions'),
+    orderby = document.querySelector('.orderby'),
+    categories = document.querySelector('.categories');
+
 
 SidebarButton.addEventListener("click", () => {
     Sidebar.classList.toggle("close");
